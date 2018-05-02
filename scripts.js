96422be5aa811863d2cfdea6fd3b9f789a52080e
Will <u3142921@uni.canberra.edu.au>
2018-05-02 16:05:06 +1000

    Overhaul

--- scripts.js
+++ scripts.js
@@ -4,10 +4,20 @@ $("#start-button").click(function() {
 
 	$('#fullpage').fullpage({
     //Changing the colours of the fullpage framework
-	sectionsColor: ['#0D0E0C', '#00270A', '#0D0E0C', '#00270A'],
+	sectionsColor: ['#211E2B', '#211E2B', '#211E2B', '#211E2B'],
 	//Allowing css to be used with fullpage framework
 	css3: true
 	 });
+
+});
+
+
+myScore = [];
+
+
+
+$("#yes").click(function() {  
+    console.log($("#yes").data.type);
 
 });
 
