49ac4ca4307627b91ff27e946737efa31a6d89b7
Will <u3142921@uni.canberra.edu.au>
2018-05-03 16:06:24 +1000

    refurb

--- scripts.js
+++ scripts.js
@@ -12,12 +12,65 @@ $("#start-button").click(function() {
 });
 
 
-myScore = [];
+$('#Incorrect-one').hover(function(){
+
+$('#Incorrect-one').delay(1000).fadeOut('slow');
+document.getElementById("fullpage").style.display = "block";
+});
 
 
 
-$("#yes").click(function() {  
-    console.log($("#yes").data.type);
+$("#yes-one").click(function() {  
+    document.getElementById("fullpage").style.display = "none";
+    document.getElementById("Incorrect-one").style.display = "block";
+});
+
+
+
+$("#no-one").click(function() {  
+	document.getElementById("fullpage").style.display = "none";
+    document.getElementById("correct-one").style.display = "block";
+});
+
+
+$("#yes-two").click(function() {  
+    document.getElementById("fullpage").style.display = "none";
+    document.getElementById("correct-one").style.display = "block";
 
 });
+
+$("#no-two").click(function() {  
+	document.getElementById("fullpage").style.display = "none";
+    document.getElementById("glitch").style.display = "block";
+});
+
+
+
+$("#yes-three").click(function() {  
+    document.getElementById("fullpage").style.display = "none";
+    document.getElementById("correct-one").style.display = "block";
+    
+});
+
+
+
+$("#no-three").click(function() {  
+	document.getElementById("fullpage").style.display = "none";
+    document.getElementById("glitch").style.display = "block";
+});
+
+
+$("#yes-four").click(function() {  
+    document.getElementById("fullpage").style.display = "none";
+    document.getElementById("Incorrect-one").style.display = "block";
+    
+});
+
+
+$("#no-four").click(function() {  
+    document.getElementById("fullpage").style.display = "none";
+    document.getElementById("correct-one").style.display = "block";
+    
+});
+
 
